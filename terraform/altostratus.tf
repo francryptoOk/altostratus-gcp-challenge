31832fd345715738cd1cdb77c59377cc557727e4
francrypto <86381767+francryptoOk@users.noreply.github.com>
2024-04-16 13:50:26 +0200

    Organizing code

--- terraform/altostratus.tf
+++
@@ -1,5 +0,0 @@
-# Network Resources #
-resource "google_compute_network" "altostratus" {
-	name = "altostratus-vpc"
-	auto_create_subnetworks = false # I create manually each subnet to manage easier my virtual private cloud (VPC) #
-}
